3b5666473df8a470eb91e3c2298ffd060ea1bfd3
leeferfeefer <danbfjr@gmail.com>
2020-07-03 17:24:16 -0400

    Finished with returning stock symbols and company profile information

--- app/api/api.py
+++ app/api/api.py
@@ -1,7 +1,7 @@
 from app.api import bp
-from flask import Response, request
+from flask import Response, request, jsonify
 from flask_cors import cross_origin
-# import json
+import json
 import finnhub
 import os
 
@@ -15,10 +15,35 @@ finnhub_client = finnhub.DefaultApi(finnhub.ApiClient(configuration))
 
 @bp.route('/stocks/symbols', methods=['GET'])
 @cross_origin()
-def search():
-    print(finnhub_client.stock_symbols('US'))
-    return Response("", status=200, mimetype='text/plain')
+def stock_symbols():
+    try:
+        stock_symbols_response = finnhub_client.stock_symbols('US')
+        stock_symbols = []
+        for stock in stock_symbols_response:
+            stock_symbol = {}
+            stock_symbol['description'] = stock.description
+            stock_symbol['display_symbol'] = stock.display_symbol
+            stock_symbol['symbol'] = stock.symbol
+            stock_symbols.append(stock_symbol)
+        return jsonify(stock_symbols)
+    except Exception as e:
+        print(e)
+        return Response("Unknown server error", status=500, mimetype='text/plain')
 
+
+@bp.route('/company/profile', methods=['GET'])
+@cross_origin()
+def company_profile():
+    try:
+        stock_symbol = request.args.get('stockSymbol')
+        if stock_symbol is None:
+            return Response("stockSymbol not defined", status=500, mimetype='text/plain')
+        company_profile_response = finnhub_client.company_profile2(symbol=stock_symbol)
+        print(company_profile_response)
+        return jsonify(company_profile_response.to_dict())
+    except Exception as e:
+        print(e)
+        return Response("Unknown server error", status=500, mimetype='text/plain')
 #
 # @bp.route('/keypress', methods=['POST'])
 # @cross_origin()
